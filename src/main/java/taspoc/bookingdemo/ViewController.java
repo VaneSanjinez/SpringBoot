ee7b87bd3ec776ed42c705241719cbda2c1f0a17
Vanessa Sanjinez <vane.sanji@gmail.com>
2018-05-06 13:45:56 -0400

    Changed link for import boostrap css. Added model to set attributes date time and user name, and changed this fields on html file

--- src/main/java/taspoc/bookingdemo/ViewController.java
+++ src/main/java/taspoc/bookingdemo/ViewController.java
@@ -1,12 +1,17 @@
 package taspoc.bookingdemo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
 
 @Controller
 public class ViewController {
     @RequestMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("datetime", new Date());
+        model.addAttribute("username", "Vanessa Sanjinez");
         return "index";
     }
 }
