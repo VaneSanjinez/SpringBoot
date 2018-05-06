097328ab08f1eaedd3b99019b81b60c8df310904
Vanessa Sanjinez <vane.sanji@gmail.com>
2018-05-05 22:59:24 -0400

    REST Controllers implemented get and post methods on booking app

--- src/main/java/taspoc/bookingdemo/BookingController.java
+++ src/main/java/taspoc/bookingdemo/BookingController.java
@@ -1,16 +1,36 @@
 package taspoc.bookingdemo;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.xml.ws.RequestWrapper;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping(value = "/bookings")
 public class BookingController {
     private List<HotelBooking> bookings;
 
     public BookingController() {
         bookings = new ArrayList<>();
+        bookings.add(new HotelBooking("Marriot", 200.50, 3));
+        bookings.add(new HotelBooking("Ibis", 90, 4));
+        bookings.add(new HotelBooking("Novotel", 140.74, 7));
+        bookings.add(new HotelBooking("Marriot", 200.50, 3));
+    }
+    /*@RequestMapping(value = "/all", method = RequestMethod.GET)
+    public List<HotelBooking> getAll(){
+        return bookings;
+    }
+    @RequestMapping(value = "/affordable/{price}", method = RequestMethod.GET)
+    public List<HotelBooking> getAffordable(@PathVariable double price){
+        return bookings.stream().filter( x ->  x.getPricePerNight() <= price).collect(Collectors.toList());
+    }*/
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public  List<HotelBooking> create(@RequestBody HotelBooking hotelBooking){
+        bookings.add(hotelBooking);
 
+        return  bookings;
     }
 }
